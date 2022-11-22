a78ef9d9dc15d027fccbd8230cbf26474f100233
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2022-11-22 13:00:56 +0500

    Again changed credentials file with production mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(name: "ikram@academy", email: "thepeacedevelopers@gmail.com", password: 'admin@ikram', password_confirmation: 'admin@ikram', admin: true,confirmed: true,is_block: false)
+User.create!(name: "ikram@academy", email: "thepeacedevelopers@gmail.com", password: 'admin@ikram', password_confirmation: 'admin@ikram', role: admin,confirmed: true,is_block: false)
 
 puts "Admin created"
