61714542d00e452693a41c14fe13fa5b28d29949
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2022-11-22 14:32:30 +0500

    Trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.sendgrid_password, # This is the secret sendgrid API key which was issued during API key creation
+  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'ikramforcesacademy.up.railway.app',
   :address => 'smtp.sendgrid.net',
   :port => 587,
